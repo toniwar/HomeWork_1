a2fdd346a4d5917fb92111751d90dd3a5a8f6e0e
Tonius <toniuswar@gmail.com>
2023-02-02 18:49:37 +0300

    My homework

--- src/task_1.kt
+++ src/task_1.kt
@@ -1,25 +1,35 @@
-import java.lang.IndexOutOfBoundsException
+
 
 fun main() {
     val list = mutableListOf<Int>()
     for(i in 0..99){
         list.add(i)
     }
-    println(list.getResult(true, 10..20, 1))
+    println(list.sliceAndSum( true, true, 10,20,1))
+    //println(list.sliceAndSum(increment = 100))
 }
 
-fun List<Int>.getResult(even:Boolean = true, range: IntRange = first().. last(), increment:Int = 0):Int {
+fun List<Int>.sliceAndSum(evenOddFilter: Boolean = false, even:Boolean = false, fst:Int = 0, lst:Int = lastIndex, increment:Int = 0):Int {
+    var range:IntRange
 
-    return  try {
-        this.filter { if (even) it % 2 == 0 else it % 2 != 0 }
+        return  this.filter {
+            when{
+                evenOddFilter ->
+                    if (even) it % 2 == 0 else it % 2 != 0
+
+                else -> true
+            }
+        }
+
+            .also {
+                val last = it.lastIndex
+                println(last)
+                range = (if(fst in 0..last) fst else 0)..if(lst in 0..last) lst else last
+
+            }
             .slice(range)
-            .map { it + increment }
-            .sumOf { it }
-    } catch (e: IndexOutOfBoundsException) {
-        this.filter { if (even) it % 2 == 0 else it % 2 != 0 }
-            .map { it + increment }
-            .sumOf { it }
-    }
+            .sumOf { it+increment }
+
 }
 
 
